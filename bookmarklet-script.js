0df36d7cd19bfbf8496c0660c864589ff2a095c8
ankitjaitly <131984456+ankitjaitly@users.noreply.github.com>
2023-04-29 03:00:06 +0530

    Update bookmarklet-script.js

--- bookmarklet-script.js
+++ bookmarklet-script.js
@@ -3,17 +3,14 @@ SEND TO NOTES FROM THE EDGE
 Bookmarklet to capture URLs, titles, and any selected text to a Google Sheets form
 Credits goes to @Rich Tatum - richtatum.github.io for original source code.
 Created: 04/26/2023 08:02 AM
-Updated: 04/27/2023 11:38 AM
    Modified comment braces
    Provided fallback for meta description
    Changed var to const
    Added whitespace and trim to variables
    Employed template literals
-Updated: 03/27/2023 8:22 AM
-   Saved to GitHub
-   Removed TLD deletion
+Updated: 04/28/2023 05:15 PM
 */
-function sendToNotesFromTheEdge(){
+javascript:(function(){function sendToNotesFromTheEdge(){
    const domain=location.hostname.toString().replace(/(www\.|\.com|\.net|\.org)/g,'').replace(/^\w/,c=> c.toUpperCase());
    const utmPattern=/(\?|&)(utm_[^&=]+=[^&]*)/gi;
    const wUrl=encodeURI(location.href.replace(utmPattern,''));
@@ -32,13 +29,12 @@ function sendToNotesFromTheEdge(){
    const wSelection=document.getSelection().toString().replace(/\s+/g,' ').trim();
    const form=document.getElementById('notes-from-the-edge-form');
    const params=new URLSearchParams();
-   params.append('entry.623797088',wTitle);
-   params.append('entry.2082543780',wUrl);
-   params.append('entry.1564268661',wSelection);
-   if(description!==""){params.append('entry.1626307794', description.replace(/\s+/g,' ').trim());}
+   params.append('entry.1339630676',wTitle);
+   params.append('entry.1944091451',wUrl);
+   params.append('entry.617355156',wSelection);
+   if(description!==""){params.append('entry.973358481', description.replace(/\s+/g,' ').trim());}
    const url=`${formUrl}?${params.toString()}`;
    /* window.location.replace(url) */
    console.log(url);
 }
-sendToNotesFromTheEdge();
-
+sendToNotesFromTheEdge();}());
